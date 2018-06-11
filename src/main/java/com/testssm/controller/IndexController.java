faa9ac7b7a691823bcbe83595ee9fef988444c79
xiaolufei <2219261145@qq.com>
2018-06-11 17:32:27 +0800

    提交

--- src/main/java/com/testssm/controller/IndexController.java
+++ src/main/java/com/testssm/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 
 @Controller
 @RequestMapping("/")
@@ -12,8 +14,9 @@ public class IndexController extends BaseController {
 
     @RequestMapping("/index")
     public String index(Model model) {
-
-        return "login";
+        model.addAttribute("username","qq");
+        logger.info("测试日志打印{}",new Date().getTime());
+        return "index";
     }
 
 
---
+++ src/main/webapp/WEB-INF/frViews/index.ftl
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <metahttp-equivmetahttp-equivmetahttp-equivmetahttp-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Inserttitle here</title>
+</head>
+<body>
+<h1>${username!}</h1>
+</body>
+</html>
